92f6b8447c3aeb0f4a63c99c91cd41d674824ec3
eideard-hm <ehhernandez81@misena.edu.co>
2022-01-13 16:11:02 -0500

    Implements the render data in the views

--- inspection-api-frontend/src/app/inspection/show-inspection/show-inspection.component.ts
+++ inspection-api-frontend/src/app/inspection/show-inspection/show-inspection.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { InspectionService } from '../../services/inspection.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-show-inspection',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowInspectionComponent implements OnInit {
 
-  constructor() { }
+  inspections$ !: Observable<any[]>;
+  inspectionTypes$ !: Observable<any[]>;
+  statuses$ !: Observable<any[]>;
+
+  inspectionTypesMap: Map<number, string> = new Map();
+
+  constructor(private inspectionService: InspectionService) { }
 
   ngOnInit(): void {
+    this.inspections$ = this.inspectionService.getInspectionList();
+    this.inspectionTypes$ = this.inspectionService.getInspectionTypesList();
+    this.statuses$ = this.inspectionService.getStatusList();
+
+    this.refreshInspectionTypesMap();
+  }
+
+  refreshInspectionTypesMap(){
+    this.inspectionService.getInspectionTypesList()
+    .subscribe(data => {
+      data.forEach((item: any) => this.inspectionTypesMap.set(item.id, item.inspectionName))
+    })
   }
 
 }
