1ed8daa764a9c5bfd94c71abd261c040b443b833
eideard-hm <ehhernandez81@misena.edu.co>
2022-01-13 10:29:38 -0500

    Started the backend project, builder the api in asp.net core

---
+++ InspectionApi/InspectionApi/Database/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using InspectionApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InspectionApi.Database
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options) { }
+        public DbSet<Inspection> Inspections { get; set; }
+        public DbSet<InspectionType> InspectionTypes { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+    }
+}
